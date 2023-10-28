2906b5d76c25e11d7721242339519817a0f45c1e
Dragonsfure <ae_a18@outlook.de>
2023-10-28 17:35:05 +0200

    Added Blog routes
    Redirecting to the Proper  Controller for  better Modulation

---
+++ routes/blogroutes.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+import * as blogController from '../controllers/blogcontroller';
+
+const router = express.Router();
+
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
+
+
+export {
+    router
+}
