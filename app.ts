1f18e33857684033d81610a9065f9174c11f508d
Dragonsfure <ae_a18@outlook.de>
2023-10-29 12:43:58 +0100

    Improved the App with the connection to controller

--- app.ts
+++ app.ts
@@ -1,55 +1,43 @@
-import express from "express";
+import * as express from "express";
 import mongoose from "mongoose";
-import morgan from "morgan";
-import { Blog } from "./models/blog";
+import * as morgan from "morgan";
 import * as dotenv from "dotenv" ;
+import { router } from "./routes/blogroutes" ;
 
 dotenv.config();
 
 const app = express();
 const dbURI = process.env.MONGODB_LINK;
 
-//connect to mongodb
-mongoose
-  .connect(dbURI)
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+mongoose.connect(dbURI)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-//register view engine
-app.set("view engine", "ejs");
+// register view engine
+app.set('view engine', 'ejs');
+
 // middleware & static files
-app.use(express.static("public"));
-app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-  res.redirect("/blogs");
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({
-      createdAt: -1,
-    })
-    .then((result) => {
-      res.render("index", { title: "Home", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
+// blog routes
+app.use('/blogs', router);
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
-
+// 404 page
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
+  res.status(404).render('404', { title: '404' });
 });
